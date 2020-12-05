f3dbe9d20bcbaedd1785cb9b533dd5813eef50d4
krzys2508 <56433668+krzys2508@users.noreply.github.com>
2020-12-05 19:53:53 +0100

    changing crud to jpa

---
+++ jaz-api/src/main/java/pl/edu/pjwstk/jazapi/service/CrudService.java
@@ -0,0 +1,41 @@
+package pl.edu.pjwstk.jazapi.service;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+
+import java.awt.print.Pageable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public abstract class CrudService<T extends DbEntity> {
+    JpaRepository<T, Long> repository;
+
+    public CrudService(JpaRepository<T, Long> repository) {
+        this.repository = repository;
+    }
+
+    public List<T> getAll(int page, int size) {
+        Iterable<T> items = repository.findAll(PageRequest.of(page,size));
+        var itemList = new ArrayList<T>();
+
+        items.forEach(itemList::add);
+
+        return itemList;
+    }
+
+    public T getById(Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    public void delete(Long id) {
+        Optional<T> item = repository.findById(id);
+
+        if (item.isPresent()) {
+            repository.delete(item.orElseThrow());
+        }
+    }
+
+    public abstract T createOrUpdate(T updateEntity);
+}
